1f7daadd86b0b17de0388c9daeee2e973b560d6e
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-11 23:44:51 +0700

    add exception class

---
+++ lib/ClientException.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace TwentyToOne\PHPClient;
+
+use \Exception;
+
+/**
+ * Class ClientException
+ * @package TwentyToOne\PHPClient
+ * @author  TwentyToOne
+ */
+class ClientException extends Exception
+{
+    /**
+     * The HTTP header of the server response.
+     * @var string[]
+     */
+    protected $responseHeaders;
+
+    /**
+     * The HTTP body of the server response either as Json or string.
+     * @var mixed
+     */
+    protected $responseBody;
+
+    /**
+     * The deserialized response object
+     * @var $responseObject;
+     */
+    protected $responseObject;
+
+    /**
+     * ClientException Constructor
+     * @param string $message         Error message
+     * @param int    $code            HTTP status code
+     * @param string $responseHeaders HTTP response header
+     * @param mixed  $responseBody    HTTP body of the server response either as Json or string
+     */
+    public function __construct($message = "", $code = 0, $responseHeaders = null, $responseBody = null)
+    {
+        parent::__construct($message, $code);
+        $this->responseHeaders = $responseHeaders;
+        $this->responseBody = $responseBody;
+    }
+
+    /**
+     * Gets the HTTP response header
+     *
+     * @return string HTTP response header
+     */
+    public function getResponseHeaders()
+    {
+        return $this->responseHeaders;
+    }
+
+    /**
+     * Gets the HTTP body of the server response either as Json or string
+     *
+     * @return mixed
+     */
+    public function getResponseBody()
+    {
+        return $this->responseBody;
+    }
+
+    /**
+     * Sets the deseralized response object (during deserialization)
+     * @param mixed $obj Deserialized response object
+     * @return void
+     */
+    public function setResponseObject($obj)
+    {
+        $this->responseObject = $obj;
+    }
+
+    /**
+     * Gets the deseralized response object (during deserialization)
+     *
+     * @return mixed the deserialized response object
+     */
+    public function getResponseObject()
+    {
+        return $this->responseObject;
+    }
+}
