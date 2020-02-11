29cd6f59c5dc7c88134f5378ce7b230972c95d0b
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-11 23:44:44 +0700

    add client class

---
+++ lib/Client.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace TwentyToOne\PHPClient;
+
+/**
+ * Class Client
+ * @package  TwentyToOne\PHPClient
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
+ */
+class Client
+{
+    /**
+     * @var Callables
+     */
+    public $call;
+
+    /**
+     * @var Object deserializer
+     */
+    protected $serializer;
+
+    /**
+     * Client constructor.
+     * @param string     $token   JWT Auth token
+     * @param array|null $options HTTP custom options
+     */
+    public function __construct($token, array $options = null)
+    {
+        $GLOBALS = [
+            'accept' => 'application/json',
+            'token' => $token
+        ];
+
+        $this->call = new Callables();
+    }
+}
