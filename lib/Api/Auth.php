8bf8636b13e737e9809ec477829d09035209b6ad
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-13 22:45:42 +0700

    remove local function

--- lib/Api/Auth.php
+++ lib/Api/Auth.php
@@ -3,48 +3,21 @@
 
 namespace Offchaindata\PHPClient\Api;
 
-
-use Offchaindata\PHPClient\Support\ClientResponse;
 use Offchaindata\PHPClient\Handler\CurlFactory;
+use Offchaindata\PHPClient\Support\ClientResponse;
 
-class Auth implements ClientResponse
+class Auth extends ClientResponse
 {
     private $url ='https://offchaindata.com/api/v1/auth/me';
 
-    public $response;
+    protected $resources;
 
     public function me()
     {
         $factory = new CurlFactory();
 
-        $this->response = $factory->create("GET", $this->url);
+        $this->resources = $factory->create("GET", $this->url);
 
         return $this;
-    }
-
-    /**
-     * Return HTTP Body from cURL request
-     */
-    public function getBody()
-    {
-        // TODO: Implement getBody() method.
-    }
-
-    /**
-     * Return HTTP Headers from cURL request
-     */
-    public function getHeaders()
-    {
-
-    }
-
-    public function getContentType()
-    {
-        return curl_getinfo($this->response, CURLINFO_CONTENT_TYPE);
-    }
-
-    public function getStatusCode()
-    {
-        return curl_getinfo($this->response, CURLINFO_HTTP_CODE);
     }
 }
