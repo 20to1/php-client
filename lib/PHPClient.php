68f37e9cfeb48d1ed67be2c2ac6f37c4e146bee0
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-17 05:57:47 +0700

    remove global variables

--- lib/PHPClient.php
+++ lib/PHPClient.php
@@ -24,21 +24,6 @@ class PHPClient
      */
     public function __construct($token, array $options = null)
     {
-        $GLOBALS = [
-            'url'    => [
-                // 'auth' => 'https://offchaindata.com/api/v1/auth/me',
-                'auth' => 'localhost:9999/api/v1/auth/me',
-                'date' => 'localhost:9999/api/v1/date',
-                'holidays' => 'localhost:9999/api/v1/date/holidays',
-                'locations' => 'localhost:9999/api/v1/date/locations',
-            ],
-            'headers' => [
-                'Authorization: Bearer ' . $token,
-                'Content-Type: ' . isset($options['content-type']) ? $options['content-type'] : 'application/json',
-                'Accept: ' . isset($options['accept']) ? $options['accept'] : 'application/json',
-            ],
-        ];
-
-        $this->call = new Callables();
+        $this->call = new Callables($token, $options);
     }
 }
