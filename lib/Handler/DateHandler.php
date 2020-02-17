34b5cfaa057173da60b0c1779141281c2af981b7
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-18 04:38:55 +0700

    add date request handler

---
+++ lib/Handler/DateHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Offchaindata\Handler;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7;
+
+class DateHandler
+{
+    private $options;
+
+    public function __construct($token, array $options = [])
+    {
+        $this->options = $options;
+        $this->options['headers'] = [
+            'Authorization' => "Bearer " . $token
+        ];
+    }
+
+    public function date($params)
+    {
+        $this->options['body'] = $params;
+
+        return $this->doRequest('POST', 'localhost:9999/api/v1/date');
+    }
+
+    public function locations()
+    {
+        return $this->doRequest('GET', 'localhost:9999/api/v1/date/locations');
+    }
+
+    public function holidays($params)
+    {
+        $this->options['body'] = $params;
+
+        return $this->doRequest('POST', 'localhost:9999/api/v1/date/holidays');
+    }
+
+    public function doRequest($method, $url)
+    {
+        $client = new Client();
+
+        try {
+            $response = $client->request($method, $url, $this->options);
+        } catch (ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        return $response;
+    }
+}
