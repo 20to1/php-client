ece67d26a606761d75ed55168f0ec1eaffe4bc85
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-18 04:38:46 +0700

    add auth handler

---
+++ lib/Handler/AuthHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Offchaindata\Handler;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7;
+use Offchaindata\Supports\ClientResponse;
+
+class AuthHandler extends ClientResponse
+{
+    protected $resources;
+    private $options;
+
+    public function __construct($token, array $options = [])
+    {
+        $this->options = $options;
+        $this->options['headers'] = [
+            'Authorization' => "Bearer " . $token
+        ];
+    }
+
+    public function me()
+    {
+        $client = new Client();
+
+        try {
+            $response = $client->request("GET", 'localhost:9999/api/v1/auth/me', $this->options);
+        } catch (ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        $this->resources = $response;
+
+        return $this;
+    }
+}
