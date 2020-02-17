4c5adbdaecdf3bfb906be5d0b297206519113e70
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-18 04:38:27 +0700

    simplify method call

--- lib/PHPClient.php
+++ lib/PHPClient.php
@@ -1,29 +1,19 @@
 <?php
 
-
 namespace Offchaindata;
 
-/**
- * Class Client
- * @package  TwentyToOne\PHPClient
- * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
- */
+use Offchaindata\Handler\AuthHandler;
+use Offchaindata\Handler\DateHandler;
+use stdClass;
+
 class PHPClient
 {
     public $call;
 
-    /**
-     * @var Object deserializer
-     */
-    protected $serializer;
-
-    /**
-     * Client constructor.
-     * @param string     $token   JWT Auth token
-     * @param array|null $options HTTP custom options
-     */
-    public function __construct($token, array $options = null)
+    public function __construct($token, array $options = [])
     {
-        $this->call = new Callables($token, $options);
+        $this->call = new stdClass;
+        $this->call->auth = new AuthHandler($token, $options);
+        $this->call->date = new DateHandler($token, $options);
     }
 }
