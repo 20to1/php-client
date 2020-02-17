67a6087c3ed2f2b4c836bb7d0294f8643fec5ea5
satuuntuksemuaaja <satu.untuk.semua.aja@gmail.com>
2020-02-18 04:47:32 +0700

    remove client response

--- lib/Supports/ClientResponse.php
+++ lib/Supports/ClientResponse.php
@@ -4,15 +4,5 @@ namespace Offchaindata\Supports;
 
 class ClientResponse
 {
-	protected $resources;
 
-	public function getBodyRaw()
-	{
-		return $this->resources->getBody();
-	}
-
-	public function getBody()
-	{
-		return json_decode($this->resources->getBody(), true);
-	}
 }
